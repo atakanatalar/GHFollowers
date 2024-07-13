41a367a29f1b523d34d8f3ae23e66b0c8e9d79a3
Atakan Atalar <atakanatalar@icloud.com>
2024-07-13 14:07:09 +0300

    create project folder

---
+++ GHFollowers/GHFollowers/GHFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersApp.swift
+//  GHFollowers
+//
+//  Created by Atakan Atalar on 13.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GHFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
