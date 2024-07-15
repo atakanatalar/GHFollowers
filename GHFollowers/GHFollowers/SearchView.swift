32e775611e6efe71906851b31c4fd9e89fb8e9b6
Atakan Atalar <atakanatalar@icloud.com>
2024-07-15 14:06:38 +0300

    add logo view, text field and button for search view

--- GHFollowers/GHFollowers/SearchView.swift
+++ GHFollowers/GHFollowers/SearchView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var username: String = ""
+    
     var body: some View {
         ZStack {
-            Text("Search View")
+            Color(.systemBackground).ignoresSafeArea()
+            
+            VStack {
+                LogoView(frameWidth: 200)
+                    .padding(.top, 96)
+                    .padding(.bottom, 24)
+                    
+                TextField("Enter a username", text: $username)
+                    .modifier(CustomTextFieldModifier())
+                    .padding(.horizontal, 48)
+                    .onSubmit { print("go button tapped") }
+                
+                Spacer()
+                
+                Button {
+                    print("get followers button tapped")
+                } label: {
+                    Label("Get Followers", systemImage: "person.3")
+                        .frame(maxWidth: .infinity, maxHeight: 48)
+                }
+                .modifier(CustomButtonModifier(backgroundColor: Color(.systemGreen)))
+                .padding(.horizontal, 48)
+                .padding(.bottom, 24)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button() {
+                        dismissKeyboard()
+                    } label: { Image(systemName: "keyboard.chevron.compact.down") }
+                }
+            }
         }
     }
 }
