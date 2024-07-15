398d81a15fac89eb5bb574c6773aedb31f60f7ce
Atakan Atalar <atakanatalar@icloud.com>
2024-07-15 16:21:54 +0300

    add models

---
+++ GHFollowers/GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Atakan Atalar on 15.07.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/GHFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Atakan Atalar on 15.07.2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    let login: String
+    let avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    let publicRepos: Int
+    let publicGists: Int
+    let htmlUrl: String
+    let following: Int
+    let followers: Int
+    let createdAt: String
+}
