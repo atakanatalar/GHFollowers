9994d69044b50e96bf83a9f4fb9779899633c88a
Atakan Atalar <atakanatalar@icloud.com>
2024-07-17 12:39:47 +0300

    add empty state view

---
+++ GHFollowers/GHFollowers/View/CustomViews/EmptyStateView.swift
@@ -0,0 +1,33 @@
+//
+//  EmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Atakan Atalar on 16.07.2024.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var message: String
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(.emptyStateLogo)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 250)
+            
+            Text(message)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .lineLimit(3)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    EmptyStateView(message: "Empty State Message")
+}
