2e5981391476e7e370896ad627a6cdb09f285a5e
Atakan Atalar <atakanatalar@icloud.com>
2024-07-17 12:43:09 +0300

    add search operation

--- GHFollowers/GHFollowers/View/Screens/FollowersView.swift
+++ GHFollowers/GHFollowers/View/Screens/FollowersView.swift
@@ -14,21 +14,26 @@ struct FollowersView: View {
     @State var page: Int = 1
     @State var hasMoreFollower: Bool = true
     @State var isLoading = false
+    @State var isEmptyState = false
+    @State var searchTerm = ""
     
+    var filteredFollowers: [Follower] {
+        guard !searchTerm.isEmpty else { return followers }
+        return followers.filter { $0.login.localizedCaseInsensitiveContains(searchTerm) }
+    }
     
     let columns = Array(repeating: GridItem(.flexible()), count: 3)
     
     var body: some View {
         ZStack {
             NavigationStack {
-                ScrollView{
+                ScrollView {
                     LazyVGrid(columns: columns) {
-                        ForEach(followers, id: \.self) { follower in
+                        ForEach(filteredFollowers, id: \.self) { follower in
                             FollowersTitleView(follower: follower)
                         }
                         
-                        if hasMoreFollower {
-//                            LoadingView()
+                        if hasMoreFollower && !self.followers.isEmpty {
                             FollowersTitleView(follower: Follower(login: "More Followers", avatarUrl: ""))
                                 .onAppear {
                                     Task {
@@ -39,14 +44,21 @@ struct FollowersView: View {
                         }
                     }
                     .padding()
+                    .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search Followers")
                 }
             }
+            if isEmptyState { EmptyStateView(message: "This user doesn't have any followers, go follow them").padding(.horizontal, 40) }
             if isLoading { LoadingView() }
         }
         .navigationTitle(username)
         .navigationBarTitleDisplayMode(.large)
         .task { await getFollowers(username: username, page: page) }
         .alert(item: $alertItem, content: { $0.alert })
+        .overlay {
+            if filteredFollowers.isEmpty && !followers.isEmpty {
+                EmptyStateView(message: "No result for \"\(searchTerm)\" ").padding(.horizontal, 40)
+            }
+        }
     }
     
     private func getFollowers(username: String, page: Int) async {
@@ -55,6 +67,9 @@ struct FollowersView: View {
             let followers = try await NetworkManager.shared.fetchFollowers(username: username, page: page)
             if followers.count < 100 { hasMoreFollower = false }
             self.followers.append(contentsOf: followers)
+            if self.followers.isEmpty {
+                DispatchQueue.main.async { showEmptyStateView() }
+            }
             hideLoadingView()
         } catch {
             hideLoadingView()
@@ -64,11 +79,11 @@ struct FollowersView: View {
                 alertItem = AlertContext.defaultError
             }
         }
-//        hideLoadingView()
     }
     
     private func showLoadingView() { isLoading = true }
     private func hideLoadingView() { isLoading = false }
+    private func showEmptyStateView() { isEmptyState = true }
 }
 
 #Preview {
