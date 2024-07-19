fa80f5beddc7d18c49ebee05c6fada7d82c26fcb
Atakan Atalar <atakanatalar@icloud.com>
2024-07-19 15:24:16 +0300

    add button action for item info views

--- GHFollowers/GHFollowers/View/CustomViews/FollowerItemInfoView.swift
+++ GHFollowers/GHFollowers/View/CustomViews/FollowerItemInfoView.swift
@@ -9,33 +9,37 @@ import SwiftUI
 
 struct FollowerItemInfoView: View {
     var user: User
+    @State var isShowingFollowersView: Bool = false
     
     var body: some View {
-        VStack {
-            HStack {
-                ItemInfoView(itemInfoType: .followers, count: user.followers)
-                Spacer()
-                ItemInfoView(itemInfoType: .following, count: user.following)
+        ZStack {
+            VStack {
+                HStack {
+                    ItemInfoView(itemInfoType: .followers, count: user.followers)
+                    Spacer()
+                    ItemInfoView(itemInfoType: .following, count: user.following)
+                }
+                .padding(.top, 24)
+                .padding(.horizontal, 12)
+                
+                Button {
+                    isShowingFollowersView = true
+                } label: {
+                    Label("Get Followers", systemImage: "person.3")
+                        .frame(maxWidth: .infinity, maxHeight: 48)
+                }
+                .modifier(CustomButtonModifier(backgroundColor: Color(.systemGreen)))
+                .padding(.horizontal, 24)
+                .padding(.bottom, 24)
             }
-            .padding(.top, 24)
-            .padding(.horizontal, 12)
-            
-            Button {
-                print("Get Followers")
-            } label: {
-                Label("Get Followers", systemImage: "person.3")
-                    .frame(maxWidth: .infinity, maxHeight: 48)
-            }
-            .modifier(CustomButtonModifier(backgroundColor: Color(.systemGreen)))
-            .padding(.horizontal, 24)
-            .padding(.bottom, 24)
+            .background(Color(.secondarySystemBackground))
+            .clipShape(RoundedRectangle(cornerRadius: 18))
+            .padding(.vertical, 8)
         }
-        .background(Color(.secondarySystemBackground))
-        .clipShape(RoundedRectangle(cornerRadius: 18))
-        .padding(.vertical, 8)
+        .navigationDestination(isPresented: $isShowingFollowersView) { FollowersView(username: user.login) }
     }
 }
 
 #Preview {
-    FollowerItemInfoView(user: User(login: "", avatarUrl: "", publicRepos: 0, publicGists: 0, htmlUrl: "", following: 0, followers: 0, createdAt: ""))
+    FollowerItemInfoView(user: User(login: "", avatarUrl: "", publicRepos: 0, publicGists: 0, htmlUrl: "", following: 0, followers: 0, createdAt: Date.now))
 }
