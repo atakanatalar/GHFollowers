ba027aaed9006036dd707475aa0a44d6e2fb5410
Atakan Atalar <atakanatalar@icloud.com>
2024-07-19 15:18:47 +0300

    add favorites operations

--- GHFollowers/GHFollowers/Utilities/GFError.swift
+++ GHFollowers/GHFollowers/Utilities/GFError.swift
@@ -11,4 +11,6 @@ enum GFError: String, Error {
     case invalidUsername = "This username created an invalid request. Please try again."
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
+    case unableToFavorite = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You have already favorited this user. You must really like them."
 }
