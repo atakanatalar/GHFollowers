574c026e522b9157b8be4f28364222a26236733a
Atakan Atalar <atakanatalar@icloud.com>
2024-07-26 20:42:13 +0300

    Revert "update mock data"
    This reverts commit 39aa44bfdcc45241187110b92b09f0f99ad2f4c9.

--- GHFollowers/GHFollowers/Utilities/Helpers/Keyboard.swift
+++ GHFollowers/GHFollowers/Utilities/Helpers/Keyboard.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ResignKeyboardOnDragGesture: ViewModifier {
     var gesture = DragGesture().onChanged { _ in
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
