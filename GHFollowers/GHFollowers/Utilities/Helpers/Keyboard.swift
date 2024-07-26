a70465914d3638de140f0ea2569c30ad53ad279c
Atakan Atalar <atakanatalar@icloud.com>
2024-07-26 20:40:55 +0300

    add keyboard helper

---
+++ GHFollowers/GHFollowers/Utilities/Helpers/Keyboard.swift
@@ -0,0 +1,30 @@
+//
+//  Keyboard.swift
+//  GHFollowers
+//
+//  Created by Atakan Atalar on 25.07.2024.
+//
+
+import SwiftUI
+
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged { _ in
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return self.modifier(ResignKeyboardOnDragGesture())
+    }
+    
+    func dismissKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
