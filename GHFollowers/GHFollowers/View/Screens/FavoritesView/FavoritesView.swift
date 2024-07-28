69f0d78dee6130c3f2145cba4e805d75f3f68841
Atakan Atalar <atakanatalar@icloud.com>
2024-07-28 13:23:19 +0300

    add refreshable for favorites view

--- GHFollowers/GHFollowers/View/Screens/FavoritesView/FavoritesView.swift
+++ GHFollowers/GHFollowers/View/Screens/FavoritesView/FavoritesView.swift
@@ -34,6 +34,9 @@ struct FavoritesView: View {
         .onAppear {
             viewModel.getFavorites()
         }
+        .refreshable {
+            viewModel.getFavorites()
+        }
     }
 }
 
